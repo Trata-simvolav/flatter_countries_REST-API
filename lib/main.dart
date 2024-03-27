a6e7abf709117bbdad3f68fa2ad840fea4d0d9d4
Trata-simvolav <114504094+Trata-simvolav@users.noreply.github.com>
2024-03-27 20:32:20 +0500

    0.0.8

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// 0.0.7
+// 0.0.8
 
 import 'dart:convert';
 import 'package:flutter/material.dart';
@@ -127,53 +127,65 @@ class _MyHomePageState extends State<MyHomePage> {
                     shadowColor: Colors.black.withOpacity(0.5),
                     elevation: 5,
                     margin: const EdgeInsets.all(8),
-                    child: ListTile(
-                      contentPadding: const EdgeInsets.all(16),
-                      title: Padding(
-                        padding: const EdgeInsets.only(
-                          bottom: 17.0,
-                        ),
-                        child: Image.network(
-                          '${item['flags']['png']}',
-                          width: 170,
-                          height: 170,
-                        ),
-                      ),
-                      subtitle: Row(
-                        children: [
-                          const SizedBox(width: 16),
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  item['name']['common'],
-                                  style: TextStyle(
-                                      color: inverterPrimary,
-                                      fontSize: 22,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                                const SizedBox(height: 8),
-                                Text(
-                                  'Population: ${item['population']}',
-                                  style: TextStyle(
-                                      color: inverterPrimary, fontSize: 16),
-                                ),
-                                Text(
-                                  'Region: ${item['region']}',
-                                  style: TextStyle(
-                                      color: inverterPrimary, fontSize: 16),
-                                ),
-                                Text(
-                                  'Capital: ${item['capital'][0]}',
-                                  style: TextStyle(
-                                      color: inverterPrimary, fontSize: 16),
-                                ),
-                              ],
+                    child: Stack(
+                      children: [
+                        SizedBox(
+                          width: 310,
+                          child: ClipRRect(
+                            borderRadius: const BorderRadius.only(
+                              topLeft: Radius.circular(
+                                  15.0), // радиус левого верхнего угла
+                              topRight: Radius.circular(
+                                  15.0), // радиус правого верхнего угла
+                            ),
+                            child: Image.network(
+                              '${item['flags']['png']}',
+                              width: 170,
+                              height: 170,
+                              fit: BoxFit.cover,
                             ),
                           ),
-                        ],
-                      ),
+                        ),
+                        ListTile(
+                          contentPadding: const EdgeInsets.all(16),
+                          title: const SizedBox(height: 160),
+                          subtitle: Row(
+                            children: [
+                              const SizedBox(width: 16),
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      item['name']['common'],
+                                      style: TextStyle(
+                                          color: inverterPrimary,
+                                          fontSize: 22,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    const SizedBox(height: 8),
+                                    Text(
+                                      'Population: ${item['population']}',
+                                      style: TextStyle(
+                                          color: inverterPrimary, fontSize: 16),
+                                    ),
+                                    Text(
+                                      'Region: ${item['region']}',
+                                      style: TextStyle(
+                                          color: inverterPrimary, fontSize: 16),
+                                    ),
+                                    Text(
+                                      'Capital: ${item['capital'][0]}',
+                                      style: TextStyle(
+                                          color: inverterPrimary, fontSize: 16),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 );
