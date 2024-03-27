bdf8f949b40b4f702623ad63b5e32802259ce327
Trata-simvolav <114504094+Trata-simvolav@users.noreply.github.com>
2024-03-27 19:49:52 +0500

    0.0.5

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
-// 0.0.4
+// 0.0.5
 
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -29,18 +31,16 @@ class _MyAppState extends State<MyApp> {
       theme: _isDarkMode
           ? ThemeData(
               colorScheme: const ColorScheme.dark(
-                primary: Color.fromARGB(
-                    255, 31, 31, 31), // Задаем голубой цвет как primary
-                secondary: Color.fromARGB(
-                    255, 48, 48, 48), // Задаем зеленый цвет как secondary
+                primary: Color.fromARGB(255, 25, 25, 25),
+                secondary: Color.fromARGB(255, 53, 53, 53),
+                background: Color.fromARGB(255, 74, 74, 74),
               ),
             )
           : ThemeData(
-              colorScheme: const ColorScheme.light(
-                primary: Color.fromARGB(
-                    255, 255, 255, 255), // Задаем голубой цвет как primary
-                secondary: Color.fromARGB(
-                    255, 214, 214, 214), // Задаем зеленый цвет как secondary
+              colorScheme: const ColorScheme.dark(
+                primary: Color.fromARGB(255, 255, 255, 255),
+                secondary: Color.fromARGB(255, 247, 247, 247),
+                background: Color.fromARGB(255, 240, 240, 240),
               ),
             ),
       home: const MyHomePage(),
@@ -57,19 +57,41 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late Future<List<dynamic>> _futureData;
+
+  @override
+  void initState() {
+    super.initState();
+    _futureData = fetchPost();
+  }
+
+  Future<List<dynamic>> fetchPost() async {
+    final response =
+        await http.get(Uri.parse("https://restcountries.com/v3.1/all"));
+
+    if (response.statusCode == 200) {
+      List<dynamic> data = json.decode(response.body);
+      return data;
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    Color primaryColor = Theme.of(context).colorScheme.primary;
-    Color secondaryColor = Theme.of(context).colorScheme.secondary;
-
     return Scaffold(
-      backgroundColor: secondaryColor,
+      backgroundColor: Theme.of(context).colorScheme.secondary,
       appBar: AppBar(
-        backgroundColor: primaryColor,
-        title: const Text("Where in the world?"),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        title: Text(
+          "WHERE MY SOCS",
+          style: TextStyle(
+              color: invertedColor(Theme.of(context).colorScheme.primary)),
+        ),
         actions: [
           IconButton(
-            icon: const Icon(Icons.lightbulb_outline),
+            icon: Icon(Icons.lightbulb_outline,
+                color: invertedColor(Theme.of(context).colorScheme.primary)),
             onPressed: () {
               final _MyAppState? state =
                   context.findAncestorStateOfType<_MyAppState>();
@@ -78,12 +100,89 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
-        ),
+      body: FutureBuilder(
+        future: _futureData,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else {
+            List<dynamic> data = snapshot.data as List<dynamic>;
+            return ListView.builder(
+              itemCount: data.length,
+              itemBuilder: (context, index) {
+                Map<String, dynamic> item = data[index];
+                return Card(
+                  color: Theme.of(context).colorScheme.background,
+                  shadowColor: Colors.black.withOpacity(0.5),
+                  elevation: 5,
+                  margin: const EdgeInsets.all(8),
+                  child: ListTile(
+                    contentPadding: const EdgeInsets.all(16),
+                    title: Padding(
+                      padding: const EdgeInsets.only(
+                        bottom: 17.0,
+                      ),
+                      child: Image.network(
+                        '${item['flags']['png']}',
+                        width: 170,
+                        height: 170,
+                      ),
+                    ),
+                    subtitle: Row(
+                      children: [
+                        const SizedBox(width: 16),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                item['name']['common'],
+                                style: TextStyle(
+                                    color: invertedColor(
+                                        Theme.of(context).colorScheme.primary),
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              const SizedBox(height: 8),
+                              Text(
+                                'Population: ${item['population']}',
+                                style: TextStyle(
+                                    color: invertedColor(
+                                        Theme.of(context).colorScheme.primary),
+                                    fontSize: 16),
+                              ),
+                              Text(
+                                'Region: ${item['region']}',
+                                style: TextStyle(
+                                    color: invertedColor(
+                                        Theme.of(context).colorScheme.primary),
+                                    fontSize: 16),
+                              ),
+                              Text(
+                                'Capital: ${item['capital'][0]}',
+                                style: TextStyle(
+                                    color: invertedColor(
+                                        Theme.of(context).colorScheme.primary),
+                                    fontSize: 16),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            );
+          }
+        },
       ),
     );
+  }
+
+  Color invertedColor(Color color) {
+    return color.computeLuminance() > 0.5 ? Colors.black : Colors.white;
   }
 }
